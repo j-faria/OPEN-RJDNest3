b15936dc83049932ef25c86d7b70beb524fcc9b4
João Faria <joao.faria@astro.up.pt>
2015-02-16 11:03:36 +0000

    no hyperparameters; new priors

--- src/MyDistribution.cpp
+++ src/MyDistribution.cpp
@@ -14,51 +14,13 @@ MyDistribution::MyDistribution()
 // Sample prior hyperparameters from their respective priors
 void MyDistribution::fromPrior()
 {
-	// Median orbital period, mu_P
-	// Cauchy prior centered on 5.901 = log(365 days), scale = 1
-	// general form for location=x0, scale=c
-	// cauchy_rand = c * tan(M_PI * (randomU()-0.5)) + x0
-	// the distribution is truncated to the interval -15.3, 27.1, hence
-	// the 0.97 and 0.485 values
-	center = 5.901 + tan(M_PI*(0.97*randomU() - 0.485));
-	// Diversity of orbital periods, w_P
-	// uniform prior between 0.1 and 3
-	width = 0.1 + 2.9*randomU();
-	// Mean amplitude (metres per second) 
-	// standard Cauchy prior 
-	// again truncated to the interval -21.2, 21.2
-	mu = exp(tan(M_PI*(0.97*randomU() - 0.485)));
+	// no hyperparameters
 }
 
 
 double MyDistribution::perturb_parameters()
 {
-	double logH = 0.;
-
-	int which = randInt(3);
-
-	if(which == 0)
-	{
-		center = (atan(center - 5.901)/M_PI + 0.485)/0.97;
-		center += randh();
-		wrap(center, 0., 1.);
-		center = 5.901 + tan(M_PI*(0.97*center - 0.485));
-	}
-	else if(which == 1)
-	{
-		width += 2.9*randh();
-		wrap(width, 0.1, 3.);
-	}
-	else
-	{
-		mu = log(mu);
-		mu = (atan(mu)/M_PI + 0.485)/0.97;
-		mu += randh();
-		wrap(mu, 0., 1.);
-		mu = tan(M_PI*(0.97*mu - 0.485));
-		mu = exp(mu);
-	}
-	return logH;
+	return 0.;
 }
 
 // vec[0] = "position" (log-period)
@@ -75,22 +37,16 @@ double MyDistribution::log_pdf(const std::vector<double>& vec) const
 			vec[4] < 0. || vec[4] > 2.*M_PI)
 		return -1E300;
 
-	return  -log(2.*width) - abs(vec[0] - center)/width
-		    -log(mu) - vec[1]/mu
-		    + 2.1*log(1. - vec[3]/0.995);
+	return 0.;
 }
 
 void MyDistribution::from_uniform(std::vector<double>& vec) const
 {
-	// inverse CDF of the Laplace distribution
-	// http://en.wikipedia.org/wiki/Laplace_distribution#Cumulative_distribution_function
-	if(vec[0] < 0.5)
-		vec[0] = center + width*log(2.*vec[0]);
-	else
-		vec[0] = center - width*log(2. - 2.*vec[0]);
+	// inverse CDF of the Jeffreys distribution [0.2d - 365000d]
+	vec[0] = 0.2*pow(365000./0.2, vec[0]);
 
-	// inverse CDF of the Exponential distribution
-	vec[1] = -mu*log(1. - vec[1]);
+	// inverse CDF of the Modified Jeffreys distribution [1, 2129]
+	vec[1] = 1.*( pow(1.+2129./1., vec[1]) - 1.);
 
 	// inverse CDF of the Uniform distribution [0, 2pi]
 	vec[2] = 2.*M_PI*vec[2];
@@ -105,15 +61,11 @@ void MyDistribution::from_uniform(std::vector<double>& vec) const
 
 void MyDistribution::to_uniform(std::vector<double>& vec) const
 {
-	// CDF of the Laplace distribution
-	// http://en.wikipedia.org/wiki/Laplace_distribution#Cumulative_distribution_function
-	if(vec[0] < center)
-		vec[0] = 0.5*exp((vec[0] - center)/width);
-	else
-		vec[0] = 1. - 0.5*exp((center - vec[0])/width);
+	// CDF of the Jeffreys distribution [0.2d - 365000d]
+	vec[0] = log(vec[0]/0.2) / log(365000./0.2);
 
-	// CDF of the Exponential distribution
-	vec[1] = 1. - exp(-vec[1]/mu);
+	// CDF of the Modified Jeffreys distribution [1, 2129]
+	vec[1] = log(1.+vec[1]/1.) / log(1.+2129./1.);
 
 	// CDF of the Uniform distribution [0, 2pi]
 	vec[2] = vec[2]/(2.*M_PI);
